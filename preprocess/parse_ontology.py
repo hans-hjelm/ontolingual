18668190541638dc21fff7b51c034d9e6edde8e3
Hans Hjelm <hans.e.hjelm@gmail.com>
2017-04-20 11:39:23 +0200

    add code for parsing eurovoc xml and outputting to tsv style format

---
+++ preprocess/parse_ontology.py
@@ -0,0 +1,25 @@
+from docopt import docopt
+import xml.etree.ElementTree as ET
+
+
+def main():
+    args = docopt("""
+    Usage:
+        parse_ontology.py <path_to_xml_file>
+    """)
+    parse_ontology(args['<path_to_xml_file>'])
+
+
+def parse_ontology(path_to_xml_file):
+    tree = ET.parse(path_to_xml_file)
+    root = tree.getroot()
+    for record in root.findall('RECORD'):
+        libelle = record.find('LIBELLE').text.lower()
+        descripteur_id = record.find('DESCRIPTEUR_ID').text
+        if 'translation' in libelle:
+            continue
+        print(libelle, descripteur_id, sep='\t', end='\n')
+
+
+if __name__ == '__main__':
+    main()
