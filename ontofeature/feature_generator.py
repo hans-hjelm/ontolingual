1a2db73f3be4f7a2c4c2b463569163c7c8ca8b8d
Hans Hjelm <hans.e.hjelm@gmail.com>
2017-04-25 11:06:06 +0200

    first round of bugfixes in feature generation

--- ontofeature/feature_generator.py
+++ ontofeature/feature_generator.py
@@ -31,39 +31,39 @@ class FeatureGenerator:
         cf = open('cohyp_features.tsv', 'w')
         with open(wordlist) as wl:
             for line in wl:
-                parts = line.split('\t')
+                parts = line.strip().split('\t')
                 words.add(parts[0])
                 self.wfreq[parts[0]] = parts[1]
         for word1 in words:
             for word2 in words:
-                id1 = word1.rsplit(maxsplit=1)[1]
-                id2 = word2.rsplit(maxsplit=1)[1]
+                id1 = word1.rsplit('#', maxsplit=1)[1]
+                id2 = word2.rsplit('#', maxsplit=1)[1]
                 if id1 == id2:
                     continue
-                hyper_example = self.get_hyper_features(word1, word2, id1, id2)
+                common_features = self.get_common_features(word1, word2)
+                hyper_example = self.get_hyper_features(id1, id2) + common_features
                 hf.write(hyper_example + '\n')
-                if int(id1) < int(id2):
-                    cohyp_example = self.get_cohyp_features(word1, word2, id1, id2)
+                if id1 < id2:
+                    cohyp_example = self.get_cohyp_features(id1, id2) + common_features
                     cf.write(cohyp_example + '\n')
         hf.close()
         cf.close()
 
-    def get_hyper_features(self, word1, word2, id1, id2):
+    def get_hyper_features(self, id1, id2):
         hyper_example = id1 + '-' + id2
-        if id2 in self.broad_to_narrow[id1]:
+        if id1 in self.broad_to_narrow.keys() and id2 in self.broad_to_narrow[id1]:
             hyper_example += '\t' + '1'
         else:
             hyper_example += '\t' + '0'
-        hyper_example += self.get_common_features(word1, word2)
         return hyper_example
 
-    def get_cohyp_features(self, word1, word2, id1, id2):
+    def get_cohyp_features(self, id1, id2):
         cohyp_example = id1 + '-' + id2
-        if id2 in self.broad_to_narrow[self.narrow_to_broad[id1]]:
+        if id1 in self.narrow_to_broad.keys() and self.narrow_to_broad[id1] in self.broad_to_narrow.keys() and \
+                id2 in self.broad_to_narrow[self.narrow_to_broad[id1]]:
             cohyp_example += '\t' + '1'
         else:
             cohyp_example += '\t' + '0'
-        cohyp_example += self.get_common_features(word1, word2)
         return cohyp_example
 
     def get_common_features(self, word1, word2):
@@ -73,7 +73,7 @@ class FeatureGenerator:
         example += '\t' + str(self.hwf.get_svd_similarity(word1, word2))
         v1 = self.hwf.get_raw_vector(word1)
         v2 = self.hwf.get_raw_vector(word2)
-        example += '\t' + ontf.context_subsumption(v1, v2)
+        example += '\t' + str(ontf.context_subsumption(v1, v2))
         ent1 = ontf.entropy(v1)
         ent2 = ontf.entropy(v2)
         example += '\t' + str(ent1) + '\t' + str(ent2) + '\t' + str(ent1 - ent2)
@@ -86,7 +86,7 @@ def main():
         feature_generator.py <path_to_ontology> <path_to_hyperwords_dir>
     """)
     fg = FeatureGenerator(args['<path_to_ontology>'], args['<path_to_hyperwords_dir>'])
-    fg.write_features('/home/hans/workspace/ontoligual/preprocess/term_freqs_22.tsv')
+    fg.write_features('/home/hans/workspace/ontolingual/preprocess/term_freqs_22.tsv')
 
 
 if __name__ == '__main__':
--- ontofeature/ontofeature.py
+++ ontofeature/ontofeature.py
@@ -2,16 +2,13 @@ import scipy.stats
 
 
 def context_subsumption(context_vector1, context_vector2):
-    if len(context_vector1) != len(context_vector2):
-        raise ValueError("Context vectors must have same length to calculate subsumption")
-    non_zero_1 = set(context_vector1.nonzero()[0])
-    non_zero_2 = set(context_vector2.nonzero()[0])
+    non_zero_1 = set(context_vector1.nonzero()[1])
+    non_zero_2 = set(context_vector2.nonzero()[1])
     non_zero_both = non_zero_1 & non_zero_2
-    if len(non_zero_1) <= len(non_zero_2):
+    if len(non_zero_1) <= len(non_zero_2) or len(non_zero_2) == 0:
         return 0
     return len(non_zero_both) / len(non_zero_2)
 
 
 def entropy(context_vector):
-    return scipy.stats.entropy(context_vector, base=2)
-
+    return scipy.stats.entropy(context_vector.data, base=2)
