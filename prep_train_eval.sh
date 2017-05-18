278e587221d73f2681af3dc80e7b144b8f368f17
Hans Hjelm <hans.e.hjelm@gmail.com>
2017-05-18 10:22:31 +0200

    add more steps to the script pipeline

--- prep_train_eval.sh
+++ prep_train_eval.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+#echo "train the hyperonym classifier"
+#python3 ontoclassifier/ontoclassifier.py data/hyper_features.tsv logistic_regression data/hyper_probs_lr.tsv
+#echo "train the cohyponym classifier"
+#python3 ontoclassifier/ontoclassifier.py data/cohyp_features.tsv logistic_regression data/cohyp_probs_lr.tsv
 echo "iteratively build the ontology"
-python3 ontobuilder/ontobuilder.py data/hyper_probs_lr_noweight.tsv data/cohyp_probs_lr_noweight.tsv data/term_freqs_22.tsv
-#echo "iteratively build the ontology"
+python3 ontobuilder/ontobuilder.py data/hyper_probs_lr.tsv data/cohyp_probs_lr.tsv data/term_freqs_22.tsv
+#echo "build the gold standard ontology"
 #python3 ontobuilder/eurovoc_ontobuilder.py <desc_thes.xml> <relation_bt.xml>
 echo "evaluate the ontology against the gold standard"
 python3 ontoeval/path_length_pmcc.py data/eurovoc_ontology.gt data/ontology.gt
